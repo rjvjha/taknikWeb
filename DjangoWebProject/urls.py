9a036a9c420e8a9a6f75dee7e21e5e43b70295c5
rjvjha1996 <rjvjha1996@gmail.com>
2015-09-24 13:38:22 +0530

    Admin function enabled

--- DjangoWebProject/urls.py
+++ DjangoWebProject/urls.py
@@ -7,9 +7,9 @@ from django.conf.urls import patterns, url
 from app.forms import BootstrapAuthenticationForm
 
 # Uncomment the next lines to enable the admin:
-# from django.conf.urls import include
-# from django.contrib import admin
-# admin.autodiscover()
+from django.conf.urls import include
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -39,5 +39,5 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+     url(r'^admin/', include(admin.site.urls)),
 )
